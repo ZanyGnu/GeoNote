a16242d3397e1af82b440019011b78479822fbcf
zanygnu <ajaymartin@gmail.com>
2015-01-15 23:25:57 -0800

    adding google client api dependencies and places models

---
+++ GeoNote/app/src/main/java/geonote/app/Model/PlaceDetails.java
@@ -0,0 +1,22 @@
+package geonote.app.Model;
+
+import java.io.Serializable;
+
+import com.google.api.client.util.Key;
+
+public class PlaceDetails implements Serializable {
+
+    @Key
+    public String status;
+
+    @Key
+    public Place result;
+
+    @Override
+    public String toString() {
+        if (result!=null) {
+            return result.toString();
+        }
+        return super.toString();
+    }
+}
