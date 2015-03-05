fbd54647f106e9ffbed61305463b34b9ad340c9b
zanygnu <ajaymartin@gmail.com>
2015-03-04 20:55:38 -0800

    Get connecting to FB to work!

--- GeoNote/app/src/main/java/geonote/app/Activity/LoginActivityFB.java
+++ GeoNote/app/src/main/java/geonote/app/Activity/LoginActivityFB.java
@@ -1,21 +1,29 @@
 package geonote.app.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+import java.util.Arrays;
 
 import geonote.app.R;
 
@@ -30,35 +38,7 @@ public class LoginActivityFB extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-
-        populateLoggedInUser();
     }
-
-    private void populateLoggedInUser() {
-
-        final TextView txtUserDetails = (TextView) this.findViewById(R.id.userDetails);
-
-        final Session session = Session.getActiveSession();
-        if (session != null && session.isOpened()) {
-            // If the session is open, make an API call to get user data
-            // and define a new callback to handle the response
-            Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
-                @Override
-                public void onCompleted(GraphUser user, Response response) {
-                    // If the response is successful
-                    if (session == Session.getActiveSession()) {
-                        if (user != null) {
-                            String user_ID = user.getId();//user id
-                            String profileName = user.getName();//user's profile name
-                            txtUserDetails.setText(user.getName());
-                        }
-                    }
-                }
-            });
-            Request.executeBatchAsync(request);
-        }
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -82,19 +62,129 @@ public class LoginActivityFB extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
+
+        private static final String TAG = "FacebookFragment";
+        private UiLifecycleHelper uiHelper;
+        private Session.StatusCallback callback = new Session.StatusCallback() {
+            @Override
+            public void call(final Session session, final SessionState state, final Exception exception) {
+                onSessionStateChange(session, state, exception);
+            }
+        };
+
+        @Override
+        public View onCreateView(LayoutInflater inflater,
+                                 ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_login_fb, container, false);
+            LoginButton authButton = (LoginButton) view.findViewById(R.id.authButton);
+            authButton.setFragment(this);
+            //authButton.setReadPermissions(Arrays.asList("user_likes", "user_status"));
+
+
+            Button button = (Button) view.findViewById(R.id.buttonGetUserDetails);
+            button.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    populateLoggedInUser();
+                }
+            });
+
+            return view;
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_login_fb, container, false);
-            return rootView;
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            uiHelper = new UiLifecycleHelper(getActivity(), callback);
+            uiHelper.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            Session session = Session.getActiveSession();
+            if (session != null &&
+                    (session.isOpened() || session.isClosed()) ) {
+                onSessionStateChange(session, session.getState(), null);
+            }
+
+            uiHelper.onResume();
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            uiHelper.onActivityResult(requestCode, resultCode, data);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            uiHelper.onPause();
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            uiHelper.onDestroy();
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            uiHelper.onSaveInstanceState(outState);
+        }
+
+        private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+            if (state.isOpened()) {
+                Log.i(TAG, "Logged in...");
+            } else if (state.isClosed()) {
+                Log.i(TAG, "Logged out...");
+            }
+            else
+                Log.i(TAG, session.toString());
+        }
+
+
+        private void populateLoggedInUser() {
+
+            final TextView txtUserDetails = (TextView) this.getActivity().findViewById(R.id.userDetails);
+
+            final Session session = Session.getActiveSession();
+            if (session != null && session.isOpened()) {
+                // If the session is open, make an API call to get user data
+                // and define a new callback to handle the response
+                Request request = Request.newMeRequest(session, new Request.GraphUserCallback() {
+                    @Override
+                    public void onCompleted(GraphUser user, Response response) {
+                        // If the response is successful
+                        if (session == Session.getActiveSession()) {
+                            if (user != null) {
+                                String user_ID = user.getId();//user id
+                                String profileName = user.getName();//user's profile name
+                                txtUserDetails.setText(user.getName());
+                            }
+                        }
+                    }
+                });
+                Request.executeBatchAsync(request);
+            }
         }
     }
+
+
 }
