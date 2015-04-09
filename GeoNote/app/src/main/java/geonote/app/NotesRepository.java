e79bb0885fa8cd8fdee02a7b20a1e39d30c06b65
zanygnu <ajaymartin@gmail.com>
2015-04-08 20:50:41 -0700

    trim default notes

--- GeoNote/app/src/main/java/geonote/app/NotesRepository.java
+++ GeoNote/app/src/main/java/geonote/app/NotesRepository.java
@@ -22,8 +22,6 @@ public class NotesRepository {
     }
 
     static final LatLng VICTORS = new LatLng(47.673988, -122.121512);
-    static final LatLng ADDRESS_14714 = new LatLng(47.735090, -122.159111);
-    static final LatLng ADDRESS_14711 = new LatLng(47.734796, -122.159598);
 
     public static NotesRepository Instance;
     public HashMap<LatLng, NoteInfo> Notes = new HashMap<LatLng, NoteInfo>();
@@ -47,19 +45,7 @@ public class NotesRepository {
         this.Notes.put(VICTORS, new NoteInfo()
                 .LatLng(VICTORS)
                 .Address(getAddressFromLatLng(VICTORS))
-                .AddNote("Remember to ask for extra hot")
-                .AddNote("Ask for cups explicitly"));
-
-        this.Notes.put(ADDRESS_14711, new NoteInfo()
-                .LatLng(ADDRESS_14711)
-                .Address(getAddressFromLatLng(ADDRESS_14711))
-                .AddNote("Remember you are at home")
-                .AddNote("Need to do something."));
-
-        this.Notes.put(ADDRESS_14714, new NoteInfo()
-                .LatLng(ADDRESS_14714)
-                .Address(getAddressFromLatLng(ADDRESS_14714))
-                .AddNote("Ask them to come home for a party?"));
+                .AddNote("Remember to ask for extra hot"));
     }
 
     private Address getAddressFromLatLng(LatLng latLng)
